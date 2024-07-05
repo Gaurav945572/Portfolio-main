daa4fd7faa0bc2bf88c9c3403a9182289496c384
Gaurav945572 <95171266+Gaurav945572@users.noreply.github.com>
2024-07-05 16:02:43 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Tilt from "react-tilt";
 import ReactTilt from 'react-tilt';
+
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -9,7 +9,7 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className='xs:w-[250px] w-full'>
+  <ReactTilt className='xs:w-[250px] w-full'>
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
@@ -33,7 +33,7 @@ const ServiceCard = ({ index, title, icon }) => (
         </h3>
       </div>
     </motion.div>
-  </Tilt>
+  </ReactTilt>
 );
 
 const About = () => {
