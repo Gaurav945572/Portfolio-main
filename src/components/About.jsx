590b160f2767e84cc2905a6f3de54aee747709ae
Gaurav945572 <95171266+Gaurav945572@users.noreply.github.com>
2024-07-05 18:53:46 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactTilt from 'react-tilt';
-
+import { Tilt } from 'react-tilt';
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -9,7 +8,7 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
-  <ReactTilt className='xs:w-[250px] w-full'>
+  <Tilt className='xs:w-[250px] w-full'>
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
@@ -33,7 +32,7 @@ const ServiceCard = ({ index, title, icon }) => (
         </h3>
       </div>
     </motion.div>
-  </ReactTilt>
+  </Tilt>
 );
 
 const About = () => {
@@ -48,13 +47,11 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        
-        I thrive on challenges and love leveraging technology to craft impactful solutions. With a strong foundation in C++, Python, and JavaScript, and hands-on experience with HTML, CSS, Bootstrap, and React.js, I bring ideas to life.
-<br />
-As a full-stack web developer, I am dedicated to building visually stunning and functional websites and web applications. My passion for new technologies and frameworks, combined with a keen interest in problem-solving within data structures and algorithms, drives me forward.
-<br />
-Competitive programming has sharpened my analytical thinking, helping me excel in coding challenges on platforms like LeetCode, CodeChef, and Codeforces. I am driven by a love for continuous learning and a desire to push the boundaries of my skills.
-        
+        I'm a full-stack web developer with a passion for building beautiful
+        websites and web applications. I enjoy creating things that live on the
+        internet and I love to learn about new technologies and frameworks.
+        I'm also a avid problem solver of DSA. Currently, I'm pursuing my Bachelors
+        in Information Science at M.S. Ramaiah Institute of Technology, Bangalore.
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10 justify-center'>
