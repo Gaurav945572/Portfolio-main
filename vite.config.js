94226060122b56f96eb3749311bc858de00cb70c
Gaurav945572 <95171266+Gaurav945572@users.noreply.github.com>
2024-07-05 19:20:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-tilt/dist/tilt']
+      external: ['react-tilt', 'react-parallax-tilt']
     }
-  },
+  }
 });
